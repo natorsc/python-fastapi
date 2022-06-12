e6b0751c6c73bdcc20cfca719ba7910d0e4de479
Renato Cruz <zkpcvm6dz@mozmail.com>
2022-06-12 10:05:25 -0300

    Melhorando acesso a variáveis de ambiente.

--- v1/settings.py
+++ v1/settings.py
@@ -1,24 +1,26 @@
-import os
 from functools import lru_cache
 from pathlib import Path
 
-from dotenv import load_dotenv
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
 
 CURRENT_DIRECTORY: Path = Path(__file__).resolve().parent
 ROOT_DIR: Path = CURRENT_DIRECTORY.parent
-
-load_dotenv(dotenv_path=ROOT_DIR.joinpath('.env'), override=True)
+ENV_FILE: str = str(ROOT_DIR.joinpath('.env'))
 
 
 class Settings(BaseSettings):
-    DEBUG: str = os.getenv('DEBUG')
-    DATABASE_URL: str = os.getenv('DATABASE_URL')
-    FAVICON: Path = CURRENT_DIRECTORY.joinpath('static', 'favicon', 'justcode.ico')
+    DEBUG: str = 'True'
+    DATABASE_URL: str = 'sqlite:///dev.sqlite3'
+    FAVICON: Path = CURRENT_DIRECTORY.joinpath(
+        'static', 'favicon', 'justcode.ico'
+    )
     STATIC_PATH: Path = CURRENT_DIRECTORY.joinpath('static')
     TEMPLATES_PATH: Path = CURRENT_DIRECTORY.joinpath('templates')
     TEMPLATES: Jinja2Templates = Jinja2Templates(directory=TEMPLATES_PATH)
+
+    class Config:
+        env_file = ENV_FILE
 
 
 @lru_cache()
