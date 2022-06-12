0e131bfe13102e3ca5826c17306cd096d0a42e6e
Renato Cruz <zkpcvm6dz@mozmail.com>
2022-06-12 10:18:41 -0300

    Código inicial para conexão com o banco.

--- v1/database.py
+++ v1/database.py
@@ -1,1 +1,23 @@
-'''ToDo'''
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from settings import Settings, get_settings
+
+settings: Settings = get_settings()
+
+engine = create_engine(
+    settings.DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
